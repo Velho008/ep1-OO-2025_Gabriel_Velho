e06c9059ca871d4e1306df7335ed6bede9954ba4
Velho008 <playergueico@gmail.com>
2025-04-29 13:42:57 -0300

    expandi menu, principalmente modo aluno, mudei nome de variaveis no menu, expandi construtores de aluno

--- SIGAA2.java
+++ SIGAA2.java
@@ -8,33 +8,82 @@ public class SIGAA2
 {
         public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
+        Scanner input1 = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
+        Scanner digitos = new Scanner(System.in);
+
         boolean continuar = true;
         while(continuar)
         {
             
-            System.out.println("seja bem vindo ao menu");
+            System.out.println("MENU");
             System.out.println("digite 1 para o modo aluno");
             System.out.println("digite 2 para o modo turma");
             System.out.println("digite 3 para o modo notas");
             System.out.println("digite 0 para sair");
-            int escolha = input.nextInt();
+            int escolha = input1.nextInt();
 
             switch (escolha)
             {
                 case 1:
-                    System.out.println("escolheu 1");
-                    Aluno gabriel = new Aluno();
-                    Aluno_especial caua = new Aluno_especial();
-                    Disciplina calculo1 = new Disciplina("Calculo1", "C1", 80);
+                    System.out.println("MODO ALUNO");
+
+                    System.out.println("digite 1 para cadastrar alunos");
+                    System.out.println("digite 2 para listar os alunos cadastrados");
+                    System.out.println("digite 3 para matricular um aluno em uma disciplina");
+                    System.out.println("digite 4 para editar alunos matriculados ou editar seu cadastro");
+                    System.out.println("digite 0 para fechar o programa");
+                    System.out.println("digite outro inteiro para retornar ao menu anterior");
+                    int escolha2 = input2.nextInt();
+                    switch (escolha2) {
+                        case 1:
+                        System.out.println("MODO CADASTRO DE ALUNOS");
+                        System.out.println("digite o nome do aluno: ");
+                        String nome = digitos.nextLine();
+                        System.out.println("digite a matricula do aluno ou 0 para pular");
+                        int matricula = digitos.nextInt();
+                        if (matricula == 0)
+                        {
+                            //fazer o aluno só com nome
+                        }
+                        else
+                        {
+                            System.out.println("digite o curso do aluno");
+                            {
+                                String curso = digitos.nextLine();
+                            }
+                            System.out.println("digite o codigo das disciplinas já cursadas pelo aluno");
+                            List<String> disciplinas_cursadas = digitos.nextLine(); //arrumar isso, a pessoa deve escrever os codigos com algo separado eles e isso deve ser convertido em uma lista com as disciplinas ja cursadas
+                        }
+                            
+                            break;
+                        case 2:
+
+                            break;
+                        case 3: 
+
+                            break;
+
+                        case 0:
+                            System.exit(0);
+                    
+                        default:
+                            break;
+                    }
+
+
                     continuar = false;
                     break;
                 case 2:
-                    System.out.println("escolheu 2");
+                    System.out.println("MODO TURMA");
+
+
                     continuar = false;
                     break;
                 case 3:
-                    System.out.println("escolheu 3");
+                    System.out.println("MODO NOTAS");
+
+
                     continuar = false;
                     break;
                 case 0:
@@ -44,7 +93,8 @@ public class SIGAA2
                     System.out.println("numero invalido, digite novamente");
                     continue;
             }
-            input.close();
+            input1.close();
+            input2.close();
         }
     }
-} //teste2
+}
--- classes/Aluno.java
+++ classes/Aluno.java
@@ -6,9 +6,35 @@ public class Aluno
     private String curso;
     private String nome;
     private int matricula;
-    private List<String> disciplinas_cursadas; //serve para checar pré-requisitos
+    private List<String> disciplinas_cursadas; //serve para checar pré-requisitos, deve ser o codigo da disciplina
 
-    public Aluno(String nome, int matricula, String curso) //construtor
+
+    //construtores
+    public Aluno(String nome, int matricula, String curso, List<String> disciplinas_cursadas)
+    {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.curso = curso;
+        this.disciplinas_cursadas = disciplinas_cursadas;
+
+        System.out.println("um novo aluno foi criado");
+        System.out.println("nome: "+nome);
+        System.out.println("matricula: "+matricula);
+        System.out.println("curso: "+curso);
+        if (this.disciplinas_cursadas.isEmpty())
+        {
+            System.out.println("não cursou nenhuma disciplina");
+        }
+        else
+        {
+            System.out.println("atualmente cursou: ");
+            for (String discuplina_cursada : this.disciplinas_cursadas)
+            {
+                System.out.println(discuplina_cursada);
+            }
+        }
+    }
+    public Aluno(String nome, int matricula, String curso)
     {
         this.nome = nome;
         this.matricula = matricula;
@@ -18,10 +44,12 @@ public class Aluno
         System.out.println("nome: "+nome);
         System.out.println("matricula: "+matricula);
         System.out.println("curso: "+curso);
+        System.out.println("não cursou nenhuma disciplina");
     }
-    public Aluno() //construtor sem infos
+    public Aluno(String nome) //construtor sem infos
     {
-        System.out.println("um novo aluno sem info foi criado");
+        System.out.println("um novo aluno "+nome+" sem info foi criado");
+        this.nome = nome;
     }
 
     //setters e getters
