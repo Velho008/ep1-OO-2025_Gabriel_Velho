1f5d6b135e8444434171fe1020b5476196409678
Velho008 <playergueico@gmail.com>
2025-04-29 21:56:23 -0300

    mudança no menu, sem switch case aninhado no modo aluno

--- SIGAA2.java
+++ SIGAA2.java
@@ -10,7 +10,6 @@ public class SIGAA2
     {
         Scanner input1 = new Scanner(System.in);
         Scanner input2 = new Scanner(System.in);
-        Scanner digitos = new Scanner(System.in);
 
         boolean continuar = true;
         while(continuar)
@@ -26,51 +25,6 @@ public class SIGAA2
             switch (escolha)
             {
                 case 1:
-                    System.out.println("MODO ALUNO");
-
-                    System.out.println("digite 1 para cadastrar alunos");
-                    System.out.println("digite 2 para listar os alunos cadastrados");
-                    System.out.println("digite 3 para matricular um aluno em uma disciplina");
-                    System.out.println("digite 4 para editar alunos matriculados ou editar seu cadastro");
-                    System.out.println("digite 0 para fechar o programa");
-                    System.out.println("digite outro inteiro para retornar ao menu anterior");
-                    int escolha2 = input2.nextInt();
-                    switch (escolha2) {
-                        case 1:
-                        System.out.println("MODO CADASTRO DE ALUNOS");
-                        System.out.println("digite o nome do aluno: ");
-                        String nome = digitos.nextLine();
-                        System.out.println("digite a matricula do aluno ou 0 para pular");
-                        int matricula = digitos.nextInt();
-                        if (matricula == 0)
-                        {
-                            //fazer o aluno só com nome
-                        }
-                        else
-                        {
-                            System.out.println("digite o curso do aluno");
-                            {
-                                String curso = digitos.nextLine();
-                            }
-                            System.out.println("digite o codigo das disciplinas já cursadas pelo aluno");
-                            List<String> disciplinas_cursadas = digitos.nextLine(); //arrumar isso, a pessoa deve escrever os codigos com algo separado eles e isso deve ser convertido em uma lista com as disciplinas ja cursadas
-                        }
-                            
-                            break;
-                        case 2:
-
-                            break;
-                        case 3: 
-
-                            break;
-
-                        case 0:
-                            System.exit(0);
-                    
-                        default:
-                            break;
-                    }
-
 
                     continuar = false;
                     break;
@@ -96,5 +50,56 @@ public class SIGAA2
             input1.close();
             input2.close();
         }
+    }
+    public static void ModoAluno(Scanner input)
+    {
+        int escolha;
+
+        do //menuzinho o modo aluno
+        {
+        System.out.println("MODO ALUNO");
+        System.out.println("digite 1 para cadastrar alunos");
+        System.out.println("digite 2 para listar os alunos cadastrados");
+        System.out.println("digite 3 para matricular um aluno em uma disciplina");
+        System.out.println("digite 4 para editar alunos matriculados ou editar seu cadastro");
+        System.out.println("digite 0 para fechar o programa");
+        escolha = input.nextInt();
+
+        switch (escolha)
+        {
+            case 1:
+                // cadastro
+
+                break;
+            case 2:
+                //listar alunos
+
+                break;
+            case 3:
+                //colocar aluno em disciplina
+
+                break;
+            case 4:
+                //editar aluno
+
+                break;
+            case 0:
+                System.exit(0);
+            default:
+                System.out.println("DIGITO INVALIDO, DIGITE NOVAMENTE");
+                break;
+        }
+
+        } while (escolha !=0);
+
+    }
+
+    public static void ModoTurma(Scanner input)
+    {
+
+    }
+    public static void ModoNotas(Scanner input)
+    {
+
     }
 }
